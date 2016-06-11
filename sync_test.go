3419d88d922a60ede9588ad572b13a6f0f36cf7f
Simon Tucker <brotherlogic@gmail.com>
2016-06-11 13:08:24 -0700

    Adds a server element. This closes #8.

--- sync_test.go
+++ sync_test.go
@@ -1,16 +1,61 @@
 package main
 
+import "golang.org/x/net/context"
+import "google.golang.org/grpc"
+import "log"
+import "net"
 import "os"
 import "testing"
-import pb "github.com/brotherlogic/godiscogs"
+import pbd "github.com/brotherlogic/godiscogs"
+import pb "github.com/brotherlogic/discogssyncer/server"
 
 func TestSaveLocation(t *testing.T) {
 	syncer := Syncer{saveLocation: ".testfolder/"}
-	release := &pb.Release{Id: 1234}
+	release := &pbd.Release{Id: 1234}
 	syncer.saveRelease(release)
 
 	//Check that the file is in the right location
 	if _, err := os.Stat(".testfolder/1234.release"); os.IsNotExist(err) {
 		t.Errorf("File does not exists")
+	}
+}
+
+func GetTestSyncer() Syncer {
+	syncer := Syncer{
+		saveLocation: ".testfolder/",
+		host:         "localhost",
+		port:         "12345",
+	}
+	return syncer
+}
+
+func RunServer() {
+	go func() {
+		syncer := GetTestSyncer()
+		lis, err := net.Listen("tcp", ":"+syncer.port)
+		if err != nil {
+			log.Fatal("Failed to bring up server: %v", err)
+		}
+		s := grpc.NewServer()
+		pb.RegisterDiscogsServiceServer(s, &syncer)
+		s.Serve(lis)
+	}()
+}
+
+func TestServer(t *testing.T) {
+	RunServer()
+	conn, err := grpc.Dial("localhost:12345", grpc.WithInsecure())
+	if err != nil {
+		t.Errorf("Error connecting to server: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewDiscogsServiceClient(conn)
+
+	r, err := client.GetCollection(context.Background(), &pb.Empty{})
+	if err != nil {
+		t.Errorf("Error getting collection: %v", err)
+	}
+	if len(r.Releases) == 0 {
+		t.Errorf("Collection has come back empty")
 	}
 }
