4085c2c5d9ad459bef7f7e781ae5cd473ac66298
Simon Tucker <brotherlogic@gmail.com>
2016-06-27 18:01:25 -0700

    Added a bit to compare git hashes

--- BuildAndRun.py
+++ BuildAndRun.py
@@ -1,9 +1,12 @@
 import os
 import subprocess
 
+current_hash = os.popen('git rev-parse HEAD').readlines()[0]
 # Update to the latest version
 for line in os.popen('go get -u github.com/brotherlogic/discogssyncer').readlines():
     print line.strip()
+new_hash = os.popen('git rev-parse HEAD').readlines()[0]
+
     
 # Move the old version over
 for line in os.popen('cp discogssyncer oldsync').readlines():
@@ -20,7 +23,7 @@ for line in os.popen('go build').readlines():
 size_1 = os.path.getsize('./oldsync')
 size_2 = os.path.getsize('./discogssyncer')
 
-if size_1 != size_2:
+if size_1 != size_2 or new_hash != current_hash:
     for line in os.popen('killall discogssyncer').readlines():
         pass
     subprocess.Popen(['./discogssyncer', '--sync=false'])
