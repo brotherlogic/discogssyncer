9c419e468e92fb893a3d12ae1cdb0befe2fded75
Simon Tucker <brotherlogic@gmail.com>
2016-06-27 18:54:05 -0700

    Fixed the run script

--- BuildAndRun.py
+++ BuildAndRun.py
@@ -1,15 +1,17 @@
 import os
 import subprocess
 
+name = "discogssyncer"
+
 current_hash = os.popen('git rev-parse HEAD').readlines()[0]
 # Update to the latest version
-for line in os.popen('go get -u github.com/brotherlogic/discogssyncer').readlines():
+for line in os.popen('go get -u github.com/brotherlogic/' + name).readlines():
     print line.strip()
 new_hash = os.popen('git rev-parse HEAD').readlines()[0]
 
     
 # Move the old version over
-for line in os.popen('cp discogssyncer oldsync').readlines():
+for line in os.popen('cp ' + name + ' old' + name).readlines():
     print line.strip()
 
 # Rebuild
@@ -20,13 +22,13 @@ for line in os.popen('go build ./...').readlines():
 for line in os.popen('go build').readlines():
     print line.strip()
 
-size_1 = os.path.getsize('./oldsync')
-size_2 = os.path.getsize('./discogssyncer')
+size_1 = os.path.getsize('./old' + name)
+size_2 = os.path.getsize('./' + name)
 
-running = len(os.popen('ps -ef | grep runn').readlines()) > 2
+running = len(os.popen('ps -ef | grep ' + name).readlines()) > 2
 
               
 if size_1 != size_2 or new_hash != current_hash or not running:
-    for line in os.popen('killall discogssyncer').readlines():
+    for line in os.popen('killall' + name).readlines():
         pass
-    subprocess.Popen(['./discogssyncer', '--sync=false'])
+    subprocess.Popen(['./' + name, '--sync=false'])
