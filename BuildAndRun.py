f1633152d59741b87acf4446a0d2e2dec3c5dafc
Simon Tucker <brotherlogic@gmail.com>
2016-06-30 15:24:11 -0700

    Quick fix to run script

--- BuildAndRun.py
+++ BuildAndRun.py
@@ -29,6 +29,6 @@ running = len(os.popen('ps -ef | grep ' + name).readlines()) > 2
 
               
 if size_1 != size_2 or new_hash != current_hash or not running:
-    for line in os.popen('killall' + name).readlines():
+    for line in os.popen('killall ' + name).readlines():
         pass
     subprocess.Popen(['./' + name, '--sync=false'])
