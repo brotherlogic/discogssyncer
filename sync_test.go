cb8630598e51d594123d16ba40bd9143c3f5bcb7
Simon Tucker <brotherlogic@gmail.com>
2016-07-05 13:58:45 -0700

    Syncs the folder list on collection save. This closes #38.

--- sync_test.go
+++ sync_test.go
@@ -10,37 +10,43 @@ import pbd "github.com/brotherlogic/godiscogs"
 type testDiscogsRetriever struct{}
 
 func (testDiscogsRetriever) GetCollection() []pbd.Release {
-     var releases = make([]pbd.Release, 0)
-     releases = append(releases, pbd.Release{FolderId: 23, Id: 25})
-      releases = append(releases, pbd.Release{FolderId: 23, Id: 32})
-     return releases
+	var releases = make([]pbd.Release, 0)
+	releases = append(releases, pbd.Release{FolderId: 23, Id: 25})
+	releases = append(releases, pbd.Release{FolderId: 23, Id: 32})
+	return releases
+}
+
+func (testDiscogsRetriever) GetFolders() []pbd.Folder {
+	var folders = make([]pbd.Folder, 0)
+	folders = append(folders, pbd.Folder{Id: 23, Name: "Testing"})
+	return folders
 }
 
 func TestSaveCollection(t *testing.T) {
-     syncer := Syncer{saveLocation: ".testcollectionsave/"}
-     syncer.SaveCollection(&testDiscogsRetriever{})
+	syncer := Syncer{saveLocation: ".testcollectionsave/"}
+	syncer.SaveCollection(&testDiscogsRetriever{})
 }
 
 func TestGetCollection(t *testing.T) {
-     syncer := Syncer{saveLocation: ".testcollectionsave/"}
-     syncer.SaveCollection(&testDiscogsRetriever{})
+	syncer := Syncer{saveLocation: ".testcollectionsave/"}
+	syncer.SaveCollection(&testDiscogsRetriever{})
 
-     releases, err := syncer.GetCollection(context.Background(), &pb.Empty{})
-     if err != nil {
-     	t.Errorf("Error returned on Get Collection")
-     }
+	releases, err := syncer.GetCollection(context.Background(), &pb.Empty{})
+	if err != nil {
+		t.Errorf("Error returned on Get Collection")
+	}
 
-     if len(releases.Releases) == 0 {
-     	t.Errorf("No releases have been returned")
-     }
+	if len(releases.Releases) == 0 {
+		t.Errorf("No releases have been returned")
+	}
 }
 
 func TestRetrieveEmptyCollection(t *testing.T) {
-     syncer := Syncer{saveLocation: ".testemptyfolder/"}
-     _, err := syncer.GetReleasesInFolder(context.Background(), &pbd.Folder{Name: "TestOne", Id: 1234})
-     if err == nil {
-     	t.Errorf("Pull from empty folder returns no error!")
-     }
+	syncer := Syncer{saveLocation: ".testemptyfolder/"}
+	_, err := syncer.GetReleasesInFolder(context.Background(), &pbd.Folder{Name: "TestOne", Id: 1234})
+	if err == nil {
+		t.Errorf("Pull from empty folder returns no error!")
+	}
 }
 
 func TestSaveLocation(t *testing.T) {
@@ -81,10 +87,9 @@ func TestSaveAndRefreshMetadata(t *testing.T) {
 	syncer.saveRelease(release, 12)
 	_, metadata2 := syncer.GetRelease(1234, 12)
 	if metadata2.DateRefreshed == metadata.DateRefreshed {
-	t.Errorf("Metadata has not been refreshed")
+		t.Errorf("Metadata has not been refreshed")
 	}
 }
-
 
 func GetTestSyncer(foldername string) Syncer {
 	syncer := Syncer{
@@ -95,9 +100,9 @@ func GetTestSyncer(foldername string) Syncer {
 
 func TestGetFolders(t *testing.T) {
 	syncer := GetTestSyncer(".testgetfolders/")
-	var folders []*pbd.Folder
-	folders = append(folders, &pbd.Folder{Name: "TestOne", Id: 1234})
-	syncer.SaveFolders(folders)
+	folderList := &pb.FolderList{}
+	folderList.Folders = append(folderList.Folders, &pbd.Folder{Name: "TestOne", Id: 1234})
+	syncer.SaveFolders(folderList)
 
 	release := &pbd.Release{Id: 1234}
 	syncer.saveRelease(release, 1234)
@@ -115,11 +120,11 @@ func TestGetFolders(t *testing.T) {
 
 func TestSaveFolderMetaata(t *testing.T) {
 	syncer := GetTestSyncer(".testSaveFolderMetadata/")
-	var folders []*pbd.Folder
-	folders = append(folders, &pbd.Folder{Name: "TestOne", Id: 1234})
-	folders = append(folders, &pbd.Folder{Name: "TestTwo", Id: 1232})
+	folderList := &pb.FolderList{}
+	folderList.Folders = append(folderList.Folders, &pbd.Folder{Name: "TestOne", Id: 1234})
+	folderList.Folders = append(folderList.Folders, &pbd.Folder{Name: "TestTwo", Id: 1232})
 
-	syncer.SaveFolders(folders)
+	syncer.SaveFolders(folderList)
 
 	if _, err := os.Stat(".testSaveFolderMetadata/metadata/folders"); os.IsNotExist(err) {
 		t.Errorf("Folder metedata has not been save")
