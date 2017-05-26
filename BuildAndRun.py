cf578c5865d5bd850d1e6cda35ffd8675dd137e0
Simon Tucker <brotherlogic@gmail.com>
2017-05-25 17:14:23 -0700

    Cleaned output

--- BuildAndRun.py
+++ BuildAndRun.py
@@ -32,11 +32,4 @@ running = len(os.popen('ps -ef | grep ' + name).readlines()) > 3
 if size_1 != size_2 or new_hash != current_hash or not running:
     for line in os.popen('killall ' + name).readlines():
         pass
-    for line in os.popen('cp out.txt out-' + `time.time()`).readlines():
-        pass
-    with open('out.txt', 'w') as output:
-        server = subprocess.Popen(['./' + name, '--sync=false', '--token=' + sys.argv[1]], stdout=output, stderr=output)
-        out, err = server.communicate()
-        output.write("%r" % out)
-        output.write("%r" % err)
-        output.write("%r" % server.returncode)
+    subprocess.Popen(['./' + name, '--sync=false', '--token=' + sys.argv[1]], stdout=output, stderr=output)
